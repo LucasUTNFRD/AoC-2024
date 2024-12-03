1ea5dc9a06635e9542664b6240b410ff04ecee5d
LucasUTNFRD <lucasdelgado@Lucass-MacBook-Air.local>
2024-12-03 11:04:13 -0300

    Add day 3 part 1 solution

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,12 +1,33 @@
 use anyhow::{Error, Result};
+use regex::Regex;
 
 const PUZZLE_INPUT: &str = include_str!("../../puzzle_input/day_03.txt");
 
 #[cfg(feature = "part_1")]
 fn solve_part_1(input: &str) -> Result<String, Error> {
-    let solution = input.lines().next().unwrap().replace("input", "answer");
+    // Convert to array of arrays of integers
+    // create a regex that only matches the following pattern
+    // mul(x,y) where x and y are 3 digit numbers
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    //from puzzle input extract all the mul(x,y) instructions and store only the x and y values as
+    //tuples in an array of tuples
+    let instructions: Vec<(i32, i32)> = re
+        .captures_iter(input)
+        .map(|cap| {
+            (
+                cap[1].parse::<i32>().unwrap(),
+                cap[2].parse::<i32>().unwrap(),
+            )
+        })
+        .collect();
 
-    Ok(solution)
+    let mut solution = 0;
+
+    for (x, y) in instructions {
+        solution += x * y;
+    }
+
+    Ok(solution.to_string())
 }
 
 #[cfg(feature = "part_2")]
@@ -36,17 +57,33 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-#[cfg(feature = "part_1")]
+#[cfg(test)]
 #[test]
 fn sample_part_1() {
-    const SAMPLE_INPUT_1: &str = "\
-sample part 1 input
-goes here
-like this
-";
-    const SAMPLE_ANSWER_1: &str = "sample part 1 answer";
+    let sample_input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    // Convert to array of arrays of integers
+    // create a regex that only matches the following pattern
+    // mul(x,y) where x and y are 3 digit numbers
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    //from puzzle input extract all the mul(x,y) instructions and store only the x and y values as
+    //tuples in an array of tuples
+    let instructions: Vec<(i32, i32)> = re
+        .captures_iter(sample_input)
+        .map(|cap| {
+            (
+                cap[1].parse::<i32>().unwrap(),
+                cap[2].parse::<i32>().unwrap(),
+            )
+        })
+        .collect();
 
-    assert_eq!(solve_part_1(SAMPLE_INPUT_1).unwrap(), SAMPLE_ANSWER_1);
+    let mut solution = 0;
+
+    for (x, y) in instructions {
+        solution += x * y;
+    }
+
+    assert_eq!(solution, 161);
 }
 
 #[cfg(feature = "part_2")]
