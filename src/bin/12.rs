bb81788dbc674d8348207fa9f0e894dcb1e3d8d8
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2025-02-16 15:12:02 -0300

    Add day 12 part 1

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,19 +1,141 @@
+use rustc_hash::FxHashSet as HashSet;
+
 use anyhow::{Error, Result};
 
 const PUZZLE_INPUT: &str = include_str!("../../puzzle_input/day_12.txt");
 
+#[derive(Debug)]
+struct Garden {
+    plot: Vec<Vec<char>>,
+    regions: Vec<Region>,
+}
+
+#[derive(Debug, Default)]
+struct Region {
+    area: u32,
+    perimeter: u32,
+    sides: u32,
+    crop: Vec<(usize, usize)>,
+}
+
+impl Region {
+    fn sides(&self) -> u32 {
+        //count corners for each crop
+    }
+}
+
+const DIRECTIONS: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+const CORNERS: [(i32, i32); 4] = [(-1, -1), (-1, 1), (1, -1), (1, 1)];
+
+impl Garden {
+    pub fn new(input: &str) -> Self {
+        let garden_plot: Vec<Vec<char>> =
+            input.lines().map(|line| line.chars().collect()).collect();
+        Self {
+            plot: garden_plot,
+            regions: vec![],
+        }
+    }
+
+    pub fn fencing_price(&self) -> u32 {
+        let regions = self.get_regions();
+        regions
+            .iter()
+            .map(|region| region.area * region.perimeter)
+            .sum()
+    }
+
+    pub fn fencing_price_part_two(&self) -> u32 {
+        let regions = self.get_regions();
+        regions
+            .iter()
+            .map(|region| region.area * region.sides)
+            .sum()
+    }
+
+    fn get_regions(&self) -> Vec<Region> {
+        let mut regions = Vec::new();
+        let mut visited: HashSet<(usize, usize)> = HashSet::default();
+
+        for i in 0..self.plot.len() {
+            for j in 0..self.plot[0].len() {
+                if !visited.contains(&(i, j)) {
+                    let mut region = Region::default();
+                    let plant_type = self.plot[i][j];
+
+                    self.dfs((i, j), &mut visited, plant_type, &mut region);
+
+                    regions.push(region);
+                }
+            }
+        }
+        regions
+    }
+
+    fn dfs(
+        &self,
+        pos: (usize, usize),
+        visited: &mut HashSet<(usize, usize)>,
+        plant_type: char,
+        region: &mut Region,
+    ) {
+        let (row, col) = pos;
+
+        visited.insert(pos);
+
+        region.area += 1;
+        region.crop.push(pos);
+
+        let mut cell_perimeter = 0;
+
+        for (dr, dc) in DIRECTIONS {
+            let new_row = row as i32 + dr;
+            let new_col = col as i32 + dc;
+
+            // Check if the new position is out of bounds
+            if new_row < 0
+                || new_col < 0
+                || new_row >= self.plot.len() as i32
+                || new_col >= self.plot[0].len() as i32
+            {
+                cell_perimeter += 1;
+
+                continue;
+            }
+
+            let new_row = new_row as usize;
+            let new_col = new_col as usize;
+
+            // If the adjacent cell is a different plant type, add to perimeter
+            if self.plot[new_row][new_col] != plant_type {
+                cell_perimeter += 1;
+            }
+            // If it's the same plant type and not visited, continue DFS
+            else if !visited.contains(&(new_row, new_col)) {
+                self.dfs((new_row, new_col), visited, plant_type, region);
+            }
+        }
+        region.perimeter += cell_perimeter
+    }
+}
+
 #[cfg(feature = "part_1")]
 fn solve_part_1(input: &str) -> Result<String, Error> {
-    let solution = input.lines().next().unwrap().replace("input", "answer");
+    let garden = Garden::new(input);
 
-    Ok(solution)
+    let total_price = garden.fencing_price();
+
+    Ok(total_price.to_string())
 }
 
 #[cfg(feature = "part_2")]
 fn solve_part_2(input: &str) -> Result<String, Error> {
-    let solution = input.lines().next().unwrap().replace("input", "answer");
+    let garden = Garden::new(input);
 
-    Ok(solution)
+    let total_price = garden.fencing_price();
+
+    Ok(total_price.to_string())
 }
 
 fn main() -> Result<(), Error> {
@@ -40,11 +162,18 @@ fn main() -> Result<(), Error> {
 #[test]
 fn sample_part_1() {
     const SAMPLE_INPUT_1: &str = "\
-sample part 1 input
-goes here
-like this
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
 ";
-    const SAMPLE_ANSWER_1: &str = "sample part 1 answer";
+    const SAMPLE_ANSWER_1: &str = "1930";
 
     assert_eq!(solve_part_1(SAMPLE_INPUT_1).unwrap(), SAMPLE_ANSWER_1);
 }
@@ -53,11 +182,19 @@ like this
 #[test]
 fn sample_part_2() {
     const SAMPLE_INPUT_2: &str = "\
-sample part 2 input
-goes here
-like this
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
 ";
-    const SAMPLE_ANSWER_2: &str = "sample part 2 answer";
+
+    const SAMPLE_ANSWER_2: &str = "1930";
 
     assert_eq!(solve_part_2(SAMPLE_INPUT_2).unwrap(), SAMPLE_ANSWER_2);
 }
