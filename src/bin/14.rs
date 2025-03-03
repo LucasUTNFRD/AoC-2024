1ab2ee85db8c25c1342a14ec4e933d3dc2c6743c
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2025-03-03 13:53:06 -0300

    Add day 14 part 1

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,12 +1,66 @@
 use anyhow::{Error, Result};
-
+use std::cmp::Ordering::*;
 const PUZZLE_INPUT: &str = include_str!("../../puzzle_input/day_14.txt");
+
+struct robots {
+    px: i32,
+    py: i32,
+    vx: i32,
+    vy: i32,
+}
+
+fn parse(input: &str) -> Vec<robots> {
+    // let bot_match = regex::Regex::new(r"p=<(\d+),(\d+)> v=<(\d+),(\d+)>").unwrap();
+    let bot_match = regex::Regex::new(r"^p\=(\d+),(\d+) v\=(-\d+|\d+),(-\d+|\d+)$").unwrap();
+    let mut bots = Vec::new();
+    for line in input.lines() {
+        if !line.is_empty() && bot_match.is_match(&line) {
+            let caps = bot_match.captures(&line).unwrap();
+            bots.push(robots {
+                px: caps[1].parse().unwrap(),
+                py: caps[2].parse().unwrap(),
+                vx: caps[3].parse().unwrap(),
+                vy: caps[4].parse().unwrap(),
+            });
+        }
+    }
+    bots
+}
+
+const WIDE: i32 = 101;
+const HEIGHT: i32 = 103;
+const TIMES: i32 = 100;
 
 #[cfg(feature = "part_1")]
 fn solve_part_1(input: &str) -> Result<String, Error> {
-    let solution = input.lines().next().unwrap().replace("input", "answer");
+    let bots = parse(input);
 
-    Ok(solution)
+    let mut quadrants = [0; 4];
+
+    let center_x = WIDE / 2;
+    let center_y = HEIGHT / 2;
+
+    bots.iter().for_each(|bot| {
+        let new_px = bot.px + TIMES * bot.vx;
+        let new_py = bot.py + TIMES * bot.vy;
+
+        // Wrap around the grid dimensions
+        let x = (new_px % WIDE + WIDE) % WIDE;
+        let y = (new_py % HEIGHT + HEIGHT) % HEIGHT;
+
+        match (x.cmp(&center_x), y.cmp(&center_y)) {
+            (Less, Less) => quadrants[0] += 1,
+            (Less, Greater) => quadrants[1] += 1,
+            (Greater, Less) => quadrants[2] += 1,
+            (Greater, Greater) => quadrants[3] += 1,
+            _ => (),
+        }
+    });
+
+    // the solution is the product of all the quadrants
+    let solution: i32 = quadrants.iter().product();
+
+    Ok(solution.to_string())
 }
 
 #[cfg(feature = "part_2")]
@@ -40,11 +94,20 @@ fn main() -> Result<(), Error> {
 #[test]
 fn sample_part_1() {
     const SAMPLE_INPUT_1: &str = "\
-sample part 1 input
-goes here
-like this
+p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
 ";
-    const SAMPLE_ANSWER_1: &str = "sample part 1 answer";
+    const SAMPLE_ANSWER_1: &str = "12";
 
     assert_eq!(solve_part_1(SAMPLE_INPUT_1).unwrap(), SAMPLE_ANSWER_1);
 }
